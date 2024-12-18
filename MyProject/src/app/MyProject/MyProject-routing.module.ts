af93a896f7dd97e42aae83ce1eddaf08fc1de6bd
vy-softuni <shark_invest@abv.bg>
2024-12-18 23:46:13 +0200

    Some more structure

---
+++ MyProject/src/app/MyProject/MyProject-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MyProjectRoutingModule { }
---
+++ MyProject/src/app/MyProject/MyProject.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyProjectRoutingModule } from './MyProject-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MyProjectRoutingModule
+  ]
+})
+export class MyProjectModule { }
