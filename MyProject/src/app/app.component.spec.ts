7ca20b46ed6e7797c338b3d807ed3bde20347e0d
vy-softuni <shark_invest@abv.bg>
2024-12-19 05:46:57 +0200

    putting the structure in order and advancing on different components

--- MyProject/src/app/app.component.spec.ts
+++ MyProject/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MyProject' title`, () => {
+  it(`should have as title 'store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyProject');
+    expect(app.title).toEqual('store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MyProject');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('store app is running!');
   });
 });
